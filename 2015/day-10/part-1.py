dbb6901d6dea6afb784a28cc52e08db8dfaba8a6
mhVxVNMwXkm9s4Ze9VASei <jediaz95@protonmail.com>
2024-09-03 09:20:42 -0500

    2015 day 10.

---
+++ 2015/day-10/part-1.py
@@ -0,0 +1,26 @@
+def generate_sequence(input_str):
+    count = 0
+    last_char = input_str[0]
+    new_sequence = ""
+
+    for i in range(len(input_str)):
+        char = input_str[i]
+
+        if char != last_char:
+            new_sequence += f"{count}{last_char}"
+            count = 1
+            last_char = char
+        else:
+            count += 1
+
+    new_sequence += f"{count}{last_char}"
+
+    return new_sequence
+
+
+input_str = "1113222113"
+
+for _ in range(40):
+    input_str = generate_sequence(input_str)
+
+print(len(input_str))
